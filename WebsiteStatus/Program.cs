220b1fedffef5120e563ad1b1852c0c6476d2d2e
OLABAYO BALOGUN <olabayobalogun@gmail.com>
2021-08-13 11:48:54 +0100

    Added serilog to the program class and converted the worker service to a windows service.

--- WebsiteStatus/Program.cs
+++ WebsiteStatus/Program.cs
@@ -4,6 +4,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
 
 namespace WebsiteStatus
 {
@@ -11,14 +13,44 @@ namespace WebsiteStatus
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //This is the configuration that ensures that you're able to override Microsoft logger features and create a solution that logs to a specific file path.
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .Enrich.FromLogContext()
+                .WriteTo.File(@"C:\Users\Public\Documents\WorkerService\AfrotadaLogfile.txt")
+                .CreateLogger();
+            
+            //This starts the logging process
+            try
+            {
+                Log.Information("Starting up the service");
+                CreateHostBuilder(args).Build().Run();
+                return;
+            }
+            catch (Exception ex)
+            {
+                Log.Fatal(ex, "There was a problem starting the service");
+                return;
+            }
+            finally
+            {
+                //This ensures that any message that is buffered or in transit is logged before the close of the application.
+                Log.CloseAndFlush();
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) =>
-                {
-                    services.AddHostedService<Worker>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                //This configures the service to run as a windows service so it can be deployed.
+               .UseWindowsService()
+               .ConfigureServices((hostContext, services) =>
+               {
+                   services.AddHostedService<Worker>();
+               })
+           //We declare this so the program knows to use Serilog
+           .UseSerilog();
+        }
     }
 }
